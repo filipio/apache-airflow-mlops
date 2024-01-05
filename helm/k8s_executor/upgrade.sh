32cd8129b24e9aba1c10f844221566d9b096679d
Filip Juza <filip.juza@placewise.com>
2024-01-05 12:19:26 +0100

    fix issue with upgrades, rename dag files

--- helm/k8s_executor/upgrade.sh
+++ helm/k8s_executor/upgrade.sh
@@ -1,2 +1,2 @@
 helm template airflow apache-airflow/airflow --create-namespace --namespace airflow --values ../values.yaml --values ../common-override-values.yaml --values ./override-values.yaml --output-dir ./generated_yamls
-helm upgrade airflow apache-airflow/airflow --values ../values.yaml --values ../common-override-values.yaml --values ./override-values.yaml
+helm upgrade airflow apache-airflow/airflow --namespace airflow --values ../values.yaml --values ../common-override-values.yaml --values ./override-values.yaml
